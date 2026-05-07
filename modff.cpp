5c727ccbd41e6b16ec150897701540741475bc8e
NamHai246357 <hainam17112010.vt@gmail.com>
2026-05-07 15:49:50 +0700

    Update modff.cpp

--- modff.cpp
+++ modff.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-extern "C" float GetHitboxScale() {
-    return 3.5f; // Tăng vùng hitbox lên 3.5 lần
+
+// Hàm khởi tạo để hệ thống nhận diện file mod
+__attribute__((constructor))
+static void initialize() {
+    std::cout << "Mod Free Fire OB53 Loaded!" << std::endl;
 }
-extern "C" bool IsAimbotEnabled() {
-    return true; // Bật chế độ tự ghim tâm
-}
+
+// Các tính năng cho anh em mình quảng cáo với khách
+extern "C" float GetHitbox() { return 3.5f; }
+extern "C" bool AutoAim() { return true; }
