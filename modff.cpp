f772b8ed90433a79d0496e8a9f7f2cb4cafe1cb9
NamHai246357 <hainam17112010.vt@gmail.com>
2026-05-07 15:52:58 +0700

    Update modff.cpp

--- modff.cpp
+++ modff.cpp
@@ -1,5 +1,2 @@
-extern "C" {
-    float GetHitboxScale() { return 3.5f; }
-    bool IsAimbotEnabled() { return true; }
-    void InitializeMod() {}
-}
+extern "C" float GetHitbox() { return 3.5f; }
+extern "C" bool IsAimbot() { return true; }
